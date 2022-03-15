10add5fb6cbc744a4305509dc85a03f65fa1c6ef
Ana Abad <ana.abadarranz@novatec-gmbh.de>
2022-03-15 16:35:16 +0100

    first commit

---
+++ src/main/java/com/example/gitTutorial/Controllers/DummyController.java
@@ -0,0 +1,17 @@
+package com.example.gitTutorial.Controllers;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DummyController {
+    @ResponseStatus(HttpStatus.OK)
+    @GetMapping("/greetings")
+    public String greetings() {
+        return "Hello there! :)";
+    }
+
+}
